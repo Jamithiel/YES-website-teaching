701e451c2a50cc243a38826c5b571955b7e6acac
Jamithiel <jamieekaplan@gmail.com>
2024-07-17 14:25:32 -0500

    Update webpage.js

--- webpage.js
+++ webpage.js
@@ -1,36 +1,4 @@
 var layout = {
-  title: {
-    text:'Graph ^_^',
-    font: {
-      family: 'Courier New, monospace',
-      size: 24
-    },
-    xref: 'paper',
-    x: 0.05,
-  },
-  paper_bgcolor: "orange",
-  xaxis: {
-    title: {
-      text: 'x Axis Line',
-      font: {
-        family: 'Courier New, monospace',
-        size: 18,
-        color: '#7f7f7f'
-      }
-    },
-  },
-  yaxis: {
-    title: {
-      text: 'y Axis Line',
-      font: {
-        family: 'Courier New, monospace',
-        size: 18,
-        color: '#7f7f7f'
-      }
-    }
-  }
-};
-
  </script> 
     <script>
 
