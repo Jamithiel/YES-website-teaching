92c29e015278c46d3052a9f96b1424414a89bfe3
Jamithiel <jamieekaplan@gmail.com>
2024-07-17 14:23:16 -0500

    Update webpage.js

--- webpage.js
+++ webpage.js
@@ -1,6 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+  <script src="https://cdn.plot.ly/plotly-2.32.0.min.js" charset="utf-8"></script>
+  <div id="tester" style="width:600px;height:250px;"></div>
+</head>
+
 var layout = {
   title: {
-    text:'Plot Title',
+    text:'Graph ^_^',
     font: {
       family: 'Courier New, monospace',
       size: 24
@@ -31,15 +43,20 @@ var layout = {
   }
 };
 
-TESTER = document.getElementById("tester");
-Plotly.newPlot(
-  TESTER,
-  [
-    {
-      x: ['red', 'green', 'orange', 'yellow', 'purple'],
-      y: [1, 2, 4, 8, 16],
-    type: 'bar'
-    }
-  ],
-  layout
-);
+ </script> 
+    <script>
+
+      TESTER = document.getElementById('tester');
+
+      Plotly.newPlot( TESTER, [{
+
+      x: ["blue 470", "cyan525", "green560", "yellow 585", "orange600", "red645", "deepred700", "IR1735", "IR2810", "IR3880", "IR4940"],
+
+      y: ["26.55%", "13.74%", "15.41%", "15.45%", "16.49%", "37.88%", "61.39%", "62.40%", "90.15%", "98.34%", "101.97%"] }], {
+
+      margin: { t: 0 } } );
+
+    </script>
+  </body> 
+  </html> 
+
